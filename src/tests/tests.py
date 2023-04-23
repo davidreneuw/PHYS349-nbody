e8bc3cdcfa41a3b9b3938344ca80ea0e6e8743b7
davidreneuw <davrene11@gmail.com>
2023-04-23 19:51:33 -0400

    Tests are good enough for now, made them write down logs. Added a bunch to the view but still need to connect to model.

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -6,7 +6,9 @@ sys.path.append('./src/v1')
 from model1 import Model1
 import assertpy as ap
 import unittest
+from datetime import datetime
 
+# Constants
 tempSystem = [
     {'time': 0, 'data': [
         {'mass': 0, 'ps': [0, 0, 0, 0, 0, 0]},
@@ -14,16 +16,23 @@ tempSystem = [
     ]}
 ]
 
+Me = copy.deepcopy(Model1.phaseTemp)
+Me['mass'] = 65
+Me['ps'] = [6_378_000, 0, 0, 0, 0, 0]
+Earth = copy.deepcopy(Model1.phaseTemp)
+Earth['mass'] = 5.92e24
+Earth['ps'] = [0, 0, 0, 0, 0, 0]
+OtherEarth = copy.deepcopy(Model1.phaseTemp)
+OtherEarth['mass'] = 5.92e24
+OtherEarth['ps'] = [2*6_378_000, 0, 0, 0, 0, 0]
+
 class TestAcceleration(unittest.TestCase):
+    """Tests relating to the acceleration function and the derivative function  """
 
     def test_magnitude(self):
+        """Tests the magnitude of the acceleration of the me on the Earth
+        """
         system = copy.deepcopy(Model1.seriesTemp)
-        Me = copy.deepcopy(Model1.phaseTemp)
-        Me['mass'] = 65
-        Me['ps'] = [6_378_000, 0, 0, 0, 0, 0]
-        Earth = copy.deepcopy(Model1.phaseTemp)
-        Earth['mass'] = 5.92e24
-        Earth['ps'] = [0, 0, 0, 0, 0, 0]
         system['data'] = [Me, Earth]
         model = Model1([system])
         test = model.dpdt([system])
@@ -35,6 +44,7 @@ class TestAcceleration(unittest.TestCase):
         self.assertAlmostEqual(np.abs(test[1][5]), 0, delta=0.01)
 
     def test_orientation(self):
+        """ Tests the orientation of the acceleration of 2 particles aligned in the x axis"""
         system = copy.deepcopy(tempSystem)
         system[0]['data'][0]['ps'] = [100, 0, 0, 0, 0, 0]
         system[0]['data'][1]['ps'] = [-100, 0, 0, 0, 0, 0]
@@ -50,17 +60,73 @@ class TestAcceleration(unittest.TestCase):
         self.assertAlmostEqual(test[1][5], 0)
 
 class TestIntegration(unittest.TestCase):
-    
-        def test_integration(self):
-            system = copy.deepcopy(tempSystem)
-            system[0]['data'][0]['ps'] = [100, 0, 0, 0, 0, 0]
-            system[0]['data'][1]['ps'] = [-100, 0, 0, 0, 0, 0]
-            system[0]['data'][0]['mass'] = 1e10
-            system[0]['data'][1]['mass'] = 1e10
-            model = Model1(system)
-            test = model.integrate(system, 1, 1)
-            self.assertAlmostEqual(test[0][0]['ps'][0], 100, delta=0.01)
-            self.assertAlmostEqual(test[0][1]['ps'][0], -100, delta=0.01)
+    """Tests relating to the Leapfrog integration method"""
+
+    def test_no_acc(self):
+        """Tests the integration of a particle with no acceleration"""
+        system = copy.deepcopy(tempSystem)
+        system[0]['data'][0]['ps'] = [0, 0, 0, 1, 0, 0]
+        system[0]['data'].pop()
+        system[0]['data'][0]['mass'] = 10
+        model = Model1(system)
+        t = np.arange(0, 10.1, 0.1)
+        solved = model.integrate(model.dpdt, t)
+        endPos = solved[-1]['data'][0]['ps'][0]
+        self.assertAlmostEqual(endPos, 10, delta=0.1)
+
+    def test_acc(self):
+        """Tests the integration of a particle with acceleration"""
+        system = copy.deepcopy(Model1.seriesTemp)
+        system['data'] = [Me, Earth]
+        model = Model1([system])
+        t = np.arange(0, 10.1, 0.1)
+        solved = model.integrate(model.dpdt, t)
+        startPos = solved[0]['data'][0]['ps'][0]
+        endPos = solved[-1]['data'][0]['ps'][0]
+        a = model.a(Me, Earth)[0]
+        kinematicAnswer = 0.5 * a * 10**2
+        self.assertAlmostEqual(endPos-startPos, kinematicAnswer, delta=0.5)
+
+    def test_conservation_energy(self):
+        """Tests the conservation of energy in the system"""
+        system = copy.deepcopy(Model1.seriesTemp)
+        system['data'] = [Me, Earth]
+        h_init = Me['ps'][0]
+        model = Model1([system])
+        a1 = model.a(Me, Earth)[0]
+        a2 = model.a(Earth, Me)[0]
+        initEnergy = Me['mass']*np.abs(a1)*h_init
+        t = np.arange(0, 30, 0.1)
+        solved = model.integrate(model.dpdt, t)
+        v_Me = np.sqrt(solved[-1]['data'][0]['ps'][3]**2 + solved[-1]['data'][0]['ps'][4]**2 + solved[-1]['data'][0]['ps'][5]**2)
+        v_Earth = np.sqrt(solved[-1]['data'][1]['ps'][3]**2 + solved[-1]['data'][1]['ps'][4]**2 + solved[-1]['data'][1]['ps'][5]**2)
+        h_endMe = np.sqrt(solved[-1]['data'][0]['ps'][0]**2 + solved[-1]['data'][0]['ps'][1]**2 + solved[-1]['data'][0]['ps'][2]**2)
+        h_endEarth = np.sqrt(solved[-1]['data'][1]['ps'][0]**2 + solved[-1]['data'][1]['ps'][1]**2 + solved[-1]['data'][1]['ps'][2]**2)
+        finalEnergy = Me['mass']*np.abs(a1)*h_endMe + Earth['mass']*np.abs(a2)*h_endEarth + 0.5*Me['mass']*v_Me**2 + 0.5*Earth['mass']*v_Earth**2
+        self.assertAlmostEqual(initEnergy, finalEnergy, delta=0.001*initEnergy)
+
+    def test_3body(self):
+        """Tests the integration of 3 particles"""
+        system = copy.deepcopy(Model1.seriesTemp)
+        system['data'] = [Earth, Me, OtherEarth]
+        model = Model1([system])
+        t = np.arange(0, 5, 0.1)
+        solved = model.integrate(model.dpdt, t)
+        dx_Me, dy_Me, dz_Me = solved[-1]['data'][1]['ps'][0]-solved[0]['data'][1]['ps'][0], solved[-1]['data'][1]['ps'][1]-solved[0]['data'][1]['ps'][1], solved[-1]['data'][1]['ps'][2]-solved[0]['data'][1]['ps'][2]
+        dx_Earth, dy_Earth, dz_Earth = solved[-1]['data'][0]['ps'][0]-solved[0]['data'][0]['ps'][0], solved[-1]['data'][0]['ps'][1]-solved[0]['data'][0]['ps'][1], solved[-1]['data'][0]['ps'][2]-solved[0]['data'][0]['ps'][2]
+        dx_OtherEarth, dy_OtherEarth, dz_OtherEarth = solved[-1]['data'][2]['ps'][0]-solved[0]['data'][2]['ps'][0], solved[-1]['data'][2]['ps'][1]-solved[0]['data'][2]['ps'][1], solved[-1]['data'][2]['ps'][2]-solved[0]['data'][2]['ps'][2]
+        disp_Me = np.sqrt(dx_Me**2 + dy_Me**2 + dz_Me**2)
+        disp_Earth = np.sqrt(dx_Earth**2 + dy_Earth**2 + dz_Earth**2)
+        disp_OtherEarth = np.sqrt(dx_OtherEarth**2 + dy_OtherEarth**2 + dz_OtherEarth**2)
+        self.assertAlmostEqual(disp_Me, 0, delta=0.1)
+        self.assertGreater(disp_Earth, 10)
+        self.assertGreater(disp_OtherEarth, 10)
+
+
 
 if __name__ == '__main__':
-    unittest.main()
+    now = str(datetime.now()).replace(" ", "_").replace(":", "_").split(".")[0]
+    log_file = f'./logs/{str(now)}.txt'
+    with open(log_file, "w") as f:
+       runner = unittest.TextTestRunner(f, verbosity=2)
+       unittest.main(testRunner=runner, verbosity=2)
---
+++ src/view/view.py
@@ -0,0 +1,111 @@
+import tkinter as tk
+from tkinter import ttk
+import copy
+class View(object):
+
+    sceneTemp = {
+        "title": "N-Body Gravitational Simulation - David René",
+        "data": [],
+        "size": (600, 200)
+    }
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.history = []
+        self.width = 600
+        self.height = 200
+        self.sceneLst = []
+        self.root.geometry(f"{self.width}x{self.height}")
+
+        btn_back = ttk.Button(self.root, text="Back", width=20, command=lambda: self.back())
+
+        lbl_select = ttk.Label(self.root, text="Please select a case study:")
+        btn_lagrange = ttk.Button(self.root, text="Lagrange points", width=20, command=lambda: self.Lagrange())
+        btn_galaxy = ttk.Button(self.root, text="Galaxy collision", width=20, command=self.Galaxy)
+        menu_scene = copy.deepcopy(self.sceneTemp)
+        menu_scene['data'] = [lbl_select, btn_lagrange, btn_galaxy]
+        self.sceneLst.append(menu_scene)
+
+        frame_lagrange = tk.Frame(self.root)
+        lbl_lagrange = ttk.Label(self.root, text="Which Lagrange point would you like to observe?")
+        ttk.Button(frame_lagrange, text="L2", width=20, command=self.L2).grid(row=0, column=0)
+        ttk.Button(frame_lagrange, text="L4", width=20, command=self.L4).grid(row=0, column=1)
+        lagrange_scene1 = copy.deepcopy(self.sceneTemp)
+        lagrange_scene1['data'] = [lbl_lagrange, frame_lagrange, btn_back]
+        lagrange_scene1['title'] = "Lagrange points - David René"
+        self.sceneLst.append(lagrange_scene1)
+
+        frame_L2 = tk.Frame(self.root)
+        tk.Canvas(frame_L2, width=600, height=400).pack(anchor=tk.CENTER, expand=True)
+        L2_scene = copy.deepcopy(self.sceneTemp)
+        L2_scene['data'] = [frame_L2, btn_back]
+        L2_scene['title'] = "L2 Observation - David René"
+        L2_scene['size'] = (600, 500)
+        self.sceneLst.append(L2_scene)
+
+        frame_L4 = tk.Frame(self.root)
+        tk.Canvas(frame_L4, width=600, height=400).pack(anchor=tk.CENTER, expand=True)
+        L4_scene = copy.deepcopy(self.sceneTemp)
+        L4_scene['data'] = [frame_L4, btn_back]
+        L4_scene['title'] = "L4 Observation - David René"
+        L4_scene['size'] = (600, 500)
+        self.sceneLst.append(L4_scene)
+
+        frame_galaxy = tk.Frame(self.root)
+        tk.Canvas(frame_galaxy, width=600, height=400).pack(anchor=tk.CENTER, expand=True)
+        galaxy_scene = copy.deepcopy(self.sceneTemp)
+        galaxy_scene['data'] = [frame_galaxy, btn_back]
+        galaxy_scene['title'] = "Galaxy Collision - David René"
+        galaxy_scene['size'] = (600, 500)
+        self.sceneLst.append(galaxy_scene)
+
+        self.build(menu_scene)
+        self.root.mainloop()
+
+    def Lagrange(self):
+        self.build(self.sceneLst[1])
+
+    def L2(self):
+        self.build(self.sceneLst[2])
+
+    def L4(self):
+        self.build(self.sceneLst[3])
+    
+    def Galaxy(self):
+        self.build(self.sceneLst[4])
+
+    def setGeo(self, size):
+        self.width = size[0]
+        self.height = size[1]
+        self.root.geometry(f"{self.width}x{self.height}")
+
+    def build(self, endScene, back=False):
+        if len(self.history) == 0:
+            startScene = None
+        else:
+            startScene = self.history[-1]
+        if not back:
+            self.history.append(endScene)
+        if startScene is not None:
+            for widget in startScene['data']:
+                widget.pack_forget()
+        self.setGeo(endScene['size'])
+        self.root.title(endScene['title'])
+        for widget in endScene['data']:
+            widget.pack(anchor=tk.CENTER, expand=True)
+        
+        return endScene
+
+    def back(self):
+        prev_scene = self.history[-2]
+        self.build(prev_scene, back=True)
+        self.history.pop(-1)
+
+    
+if __name__ == "__main__":
+    View()
+
+
+
+
+
